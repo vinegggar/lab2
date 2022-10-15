ba77dabad712576f0d2b503b5081d052b523686f
Kateryna <ketrin01092004@gmail.com>
2022-10-15 15:59:00 +0300

    added ""<<"operator; created basic class constructors

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+class Lint{
+private:
+    vector <int> digits;
+public:
+    Lint()= default;
+    Lint(string number){
+        for (int i=number.size()-1; i>=0;--i)
+            digits.push_back(number[i]-'0');
+    }
+
+    friend ostream& operator<<(ostream &out, Lint num);
+};
+
+ostream& operator<<(ostream &out, Lint num){
+    for (int i=num.digits.size()-1;i>=0;--i){
+        out <<num.digits[i];}
+    out<<endl;
+    return out;
+}
 
 int main() {
-    std::cout << "one more" << std::endl;
+    Lint A("45666"), B;
+
+    cout << A;
     return 0;
 }
