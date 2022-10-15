9c2ca210c8a7052fe9cb80d6409e62ea966aecdc
Kateryna <ketrin01092004@gmail.com>
2022-10-15 16:19:24 +0300

    added ">>"operator

--- main.cpp
+++ main.cpp
@@ -5,26 +5,36 @@ using namespace std;
 class Lint{
 private:
     vector <int> digits;
+    string val;
 public:
     Lint()= default;
-    Lint(string number){
-        for (int i=number.size()-1; i>=0;--i)
-            digits.push_back(number[i]-'0');
+    Lint(string val){
+        for (int i=int(val.size())-1; i>=0;--i)
+            digits.push_back(val[i]-'0');
     }
 
+
     friend ostream& operator<<(ostream &out, Lint num);
+    friend istream& operator>>(istream &in, Lint& num);
 };
 
 ostream& operator<<(ostream &out, Lint num){
-    for (int i=num.digits.size()-1;i>=0;--i){
+    for (int i=int(num.digits.size())-1;i>=0;--i){
         out <<num.digits[i];}
     out<<endl;
     return out;
 }
 
-int main() {
-    Lint A("45666"), B;
+istream& operator>>(istream& in, Lint& num){
+    string input;
+    in >> input;
+    num = Lint(input);
+    return in;
+}
 
-    cout << A;
+int main() {
+    Lint A("45568"),B;
+    cin>>B;
+    cout<<B<<A;
     return 0;
 }
